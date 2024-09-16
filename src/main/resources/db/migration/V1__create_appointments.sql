5e3c0d2ba4d98f67e5ab455b099c8f82d5c5f535
Gurnoor <s3991487@student.rmit.edu.au>
2024-09-16 11:20:39 +1000

    Resolved conflicts , and aded database migrations

---
+++ src/main/resources/db/migration/V1__create_appointments.sql
@@ -0,0 +1,8 @@
+CREATE TABLE appointments (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    pet_name VARCHAR(255),
+    vet_name VARCHAR(255),
+    date VARCHAR(255),
+    time VARCHAR(255),
+    status VARCHAR(255)
+);
