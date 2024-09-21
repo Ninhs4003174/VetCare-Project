fa50b267efdc3b6e90c560c9ed967adbc9e9d08c
Krishitaa <krishitaap@gmail.com>
2024-09-21 18:35:32 +1000

    fixed the "Dr." titles that were appearing twice for some vets

--- src/main/resources/db/migration/V18__insert_vets.sql
+++ src/main/resources/db/migration/V18__insert_vets.sql
@@ -1,7 +1,7 @@
 INSERT INTO VETBOOKING (NAME, CLINIC)
 VALUES
     ('Dr. John Doe,City Clinic', 'City Clinic'),
-     ('Dr. Dr. Isabella Wilson,City Clinic', 'City Clinic'),
-    ('Dr. Dr. Emily Thompson,Suburban Clinic', 'Suburban Clinic'),
+     ('Dr. Isabella Wilson,City Clinic', 'City Clinic'),
+    ('Dr. Emily Thompson,Suburban Clinic', 'Suburban Clinic'),
     ('Dr. Jane Smith,Suburban Clinic', 'Suburban Clinic');
     
