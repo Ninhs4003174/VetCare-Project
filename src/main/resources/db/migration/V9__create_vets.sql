e551b099c4e026c90c9449b756ea47cddff986e9
Gurnoor <s3991487@student.rmit.edu.au>
2024-10-01 19:01:11 +1000

    changed the vet booking table

--- src/main/resources/db/migration/V9__create_vets.sql
+++ src/main/resources/db/migration/V9__create_vets.sql
@@ -1,8 +1,12 @@
 CREATE TABLE IF NOT EXISTS vetbooking (
     id SERIAL PRIMARY KEY,                     -- Unique identifier for each booking
-    vet_user_id BIGINT NOT NULL,              -- Link to the vet user making the booking
+    vet_user_id BIGINT NOT NULL,               -- Link to the vet user making the booking
     clinic_id BIGINT NOT NULL,                 -- Clinic where the vet works, must not be null
     service_type VARCHAR(100),                 -- Type of service being booked
-    FOREIGN KEY (vet_user_id) REFERENCES vet_users(id) -- Link to the user who made the booking
-    FOREIGN KEY (clinic_id) REFERENCES vet(id) 
+    address VARCHAR(255),                     -- Address of the clinic
+    phone_number VARCHAR(20),                 -- Phone number of the clinic
+    email VARCHAR(100),                       -- Email of the clinic
+                        -- Link to the vet user account
+    FOREIGN KEY (vet_user_id) REFERENCES vet_users(id) -- Link to the vet user
+   
 );
