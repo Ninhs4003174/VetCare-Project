660c96ca3c61c8ca02f943956f77e1ebd2c3acce
Ninh Huynh <Ninhhuynh4003@gmail.com>
2024-10-12 22:20:13 +1100

    updated pet record repository

--- src/main/java/au/edu/rmit/sept/webapp/repository/PetRecordRepository.java
+++ src/main/java/au/edu/rmit/sept/webapp/repository/PetRecordRepository.java
@@ -1,10 +1,13 @@
 package au.edu.rmit.sept.webapp.repository;
 
+import au.edu.rmit.sept.webapp.model.PetRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import au.edu.rmit.sept.webapp.model.PetRecord;
+import java.util.List;
 
 @Repository
 public interface PetRecordRepository extends JpaRepository<PetRecord, Long> {
+    // Find pet records by the user ID
+    List<PetRecord> findByUserId(Long userId);
 }
